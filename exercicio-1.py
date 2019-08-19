b2262b215120e74ea161ef60558308c2f0668174
matheus s. anotnio <viciogamer157@gmail.com>
2019-08-19 17:07:16 -0300

    exercicios.py

---
+++ exercicio-1.py
@@ -0,0 +1,6 @@
+num = int(input('Qual o número???\n'))
+
+if int(num) % 2 == 0:
+    print("O numero informado e par")
+else:
+    print("O numero informado e impar")
---
+++ exercicio-2.py
@@ -0,0 +1,9 @@
+numero_celular = int(input("digite um numero valido"))
+
+if len(numero_celular == 9 and numero_celular[0] ==9):
+    print('numero valido')
+
+else:
+    print('numero invalido')
+
+    # nao to achando o erro
